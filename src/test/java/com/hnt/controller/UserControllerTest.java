fea4030d33145ac95700f9dd030b04aa4c7696bf
unknown <cogjava3195@iiht.tech>
2022-09-11 15:50:37 +0530

    First Commit

---
+++ src/test/java/com/hnt/controller/UserControllerTest.java
@@ -0,0 +1,41 @@
+package com.hnt.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.hnt.entity.User;
+import com.hnt.service.UserService;
+
+import java.util.ArrayList;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+	@Mock
+	UserService service;
+	@InjectMocks
+	UserController controller;
+
+	@Test
+	void testSaveUser1() {
+		User user = new User();
+		user.setId(1);
+		when(service.save(user)).thenReturn(user);// mocking
+		Integer savedUserId = controller.saveUser1(user);
+		assertEquals(1, savedUserId);
+	}
+
+	@Test
+	void testGetUser() {
+		Iterable<User> user = new ArrayList<>(){};
+		when(controller.getUser()).thenReturn(user);
+		Iterable<User> iterableUserReturned = controller.getUser();
+		assertEquals(iterableUserReturned,user);
+	}
+
+}
